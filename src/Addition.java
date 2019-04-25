1c0db6631d076179e57b9592ee6379c24aa4dfeb
nishanif <nlfernando11@gmail.com>
2019-04-25 12:36:42 +0530

    CAL-1 add source files

---
+++ src/Addition.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package addition;
+
+public class Addition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Add a=new Add();
+        a.setVisible(true);
+    }
+    
+}
